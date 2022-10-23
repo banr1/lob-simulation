fix calculation of amount of orders and parameter names

lob_simulation/__main__.py
@@ -11,10 +11,10 @@ from .model import Df, LimitDf, CancelDf, MarketDf, Side, Sr
 
 UNIT = 0.1
 N_ROUND = 100
-LIMIT_LAM_RANGE, LIMIT_XI_RANGE = (1, 3), (5, 10)
-CANCEL_LAM_RANGE, CANCEL_XI_RANGE = (1, 1), (1, 5)
-MARKET_LONG_LAM, MARKET_LONG_XI = 5, 50
-MARKET_SHORT_LAM, MARKET_SHORT_XI = 5, 50
+LIMIT_MU_RANGE, LIMIT_XI_RANGE = (1, 3), (5, 10)
+CANCEL_MU_RANGE, CANCEL_XI_RANGE = (1, 1), (1, 5)
+MARKET_LONG_MU, MARKET_LONG_XI = 5, 50
+MARKET_SHORT_MU, MARKET_SHORT_XI = 5, 50
 
 INIT_MID_PRICE = 100
 INIT_WIDTH=15
@@ -28,29 +28,29 @@ def main():
     ca_generators: list[CancelGenerator] = []
 
     for i, diff in enumerate(price_diffs_from_center):
-        limit_lam = np.random.uniform(*LIMIT_LAM_RANGE)
+        limit_mu = np.random.uniform(*LIMIT_MU_RANGE)
         limit_xi = np.random.uniform(*LIMIT_XI_RANGE)
-        cancel_lam = np.random.uniform(*CANCEL_LAM_RANGE)
+        cancel_mu = np.random.uniform(*CANCEL_MU_RANGE)
         cancel_xi = np.random.uniform(*CANCEL_XI_RANGE)
 
         if i != 0:
             lower_price = INIT_MID_PRICE - diff
             upper_price = INIT_MID_PRICE + diff
-            lo_generators.append(LimitOrderGenerator(lower_price, lam=limit_lam, xi=limit_xi, unit=UNIT))
-            lo_generators.append(LimitOrderGenerator(upper_price, lam=limit_lam, xi=limit_xi, unit=UNIT))
-            ca_generators.append(CancelGenerator(lower_price, lam=cancel_lam, xi=cancel_xi, unit=UNIT))
-            ca_generators.append(CancelGenerator(upper_price, lam=cancel_lam, xi=cancel_xi, unit=UNIT))
+            lo_generators.append(LimitOrderGenerator(lower_price, mu=limit_mu, xi=limit_xi, unit=UNIT))
+            lo_generators.append(LimitOrderGenerator(upper_price, mu=limit_mu, xi=limit_xi, unit=UNIT))
+            ca_generators.append(CancelGenerator(lower_price, mu=cancel_mu, xi=cancel_xi, unit=UNIT))
+            ca_generators.append(CancelGenerator(upper_price, mu=cancel_mu, xi=cancel_xi, unit=UNIT))
         # 最初だけは丁度中央なので一つだけ
         else:
-            lo_generators.append(LimitOrderGenerator(INIT_MID_PRICE, lam=limit_lam, xi=limit_xi, unit=UNIT))
-            ca_generators.append(CancelGenerator(INIT_MID_PRICE, lam=cancel_lam, xi=cancel_xi, unit=UNIT))
+            lo_generators.append(LimitOrderGenerator(INIT_MID_PRICE, mu=limit_mu, xi=limit_xi, unit=UNIT))
+            ca_generators.append(CancelGenerator(INIT_MID_PRICE, mu=cancel_mu, xi=cancel_xi, unit=UNIT))
 
-    market_long_lam = MARKET_LONG_LAM
+    market_long_mu = MARKET_LONG_MU
     market_long_xi = MARKET_LONG_XI
-    market_short_lam = MARKET_SHORT_LAM
+    market_short_mu = MARKET_SHORT_MU
     market_short_xi = MARKET_SHORT_XI
-    ml_generator = MarketOrderGenerator(Side.LONG, lam=market_long_lam, xi=market_long_xi, unit=UNIT)
-    ms_generator = MarketOrderGenerator(Side.SHORT, lam=market_short_lam, xi=market_short_xi, unit=UNIT)
+    ml_generator = MarketOrderGenerator(Side.LONG, mu=market_long_mu, xi=market_long_xi, unit=UNIT)
+    ms_generator = MarketOrderGenerator(Side.SHORT, mu=market_short_mu, xi=market_short_xi, unit=UNIT)
 
     lob = LOB(unit=UNIT)
 

lob_simulation/generator.py
@@ -11,14 +11,14 @@ class LimitOrderGenerator:
     Limit Orderを生成するクラス。
     ex)
     - price: 100
-    - lam: 5
+    - mu: 5
     - xi: 100
     - unit: 0.1
     のとき、...
     """
-    def __init__(self, price: float, lam: float, xi: float, unit: float) -> None:
+    def __init__(self, price: float, mu: float, xi: float, unit: float) -> None:
         self.price = price
-        self.lam = lam
+        self.mu = mu
         self.xi = xi
         self.unit = unit
 
@@ -29,13 +29,13 @@ class LimitOrderGenerator:
             side = Side.SHORT
         else:
             return LimitDf()
-        # 注文数: 平均lamのPoisson dist.
-        order_num = np.random.poisson(self.lam)
+        # 注文数: 平均muのPoisson dist.
+        order_num = np.random.poisson(self.mu)
         if order_num == 0:
             return LimitDf()
 
         # それぞれの注文量: 1 + 平均xiのPoisson dist.
-        order_amount = self.unit * np.random.poisson(self.xi)
+        order_amount = self.unit * (1 + np.random.poisson(self.xi))
         order_list = LimitDf(
             [LimitSr({"price": self.price, "amount": order_amount, "side": side}, name=uuid4()) for _ in range(order_num)]
         )
@@ -50,20 +50,20 @@ class MarketOrderGenerator:
     """
     Market Orderを生成するクラス。
     """
-    def __init__(self, side: Side, lam: float, xi: float, unit: float) -> None:
+    def __init__(self, side: Side, mu: float, xi: float, unit: float) -> None:
         self.side = side
-        self.lam = lam
+        self.mu = mu
         self.xi = xi
         self.unit = unit
 
     def run(self) -> MarketDf:
-        # 注文数: 平均lamのPoisson dist.
-        order_num = np.random.poisson(self.lam)
+        # 注文数: 平均muのPoisson dist.
+        order_num = np.random.poisson(self.mu)
         if order_num == 0:
             return MarketDf()
 
         # それぞれの注文量: 1 + 平均xiのPoisson dist.
-        order_amount = self.unit * np.random.poisson(self.xi)
+        order_amount = self.unit * (1 + np.random.poisson(self.xi))
         order_list = MarketDf(
             [MarketSr({"amount": order_amount, "side": self.side}, name=uuid4()) for _ in range(order_num)]
         )
@@ -75,17 +75,17 @@ class MarketOrderGenerator:
 
 
 class CancelGenerator:
-    def __init__(self, price: float, lam: float, xi: float, unit: float) -> None:
+    def __init__(self, price: float, mu: float, xi: float, unit: float) -> None:
         self.price = price
-        self.lam = lam
+        self.mu = mu
         self.xi = xi
         self.unit = unit
 
     def run(self) -> CancelDf:
-        # 注文数: 平均lamのPoisson dist.
-        order_num = np.random.poisson(self.lam)
+        # 注文数: 平均muのPoisson dist.
+        order_num = np.random.poisson(self.mu)
         # それぞれの注文量: 1 + 平均xiのPoisson dist.
-        order_amount = self.unit * np.random.poisson(self.xi)
+        order_amount = self.unit * (1 + np.random.poisson(self.xi))
         order_list = CancelDf(
             [CancelSr({"price": self.price, "amount": order_amount}, name=uuid4()) for _ in range(order_num)]
         )